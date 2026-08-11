826e0665dcfd266b4a8821db97abf5f75f6e392e
Pratima Jain <pratimajain1115@gmail.com>
2026-08-11 14:14:02 +0530

    folder structure update

--- oops/task1/animal.java
+++ oops/task1/animal.java
@@ -13,8 +13,8 @@ public class animal {
    // public void getData(){
    //     System.out.println("============ANIMAL============");
    //     System.out.println("Name of Animal is:"+name);
-   //    //   System.out.println(a1.age);
-   //    //   System.out.println(a1.color);
+   //   System.out.println(a1.age);
+   //   System.out.println(a1.color);
    // }
    public void printTotalWeight(){
       double total= weight*age;
